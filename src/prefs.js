2b157879b01108c2b5cc2523ec0134cdb34f3408
vitaminac <dalao1002@gmail.com>
2025-10-22 23:39:35 +0200

    remove redundant setting

--- src/prefs.js
+++ src/prefs.js
@@ -2,9 +2,6 @@ pref('extensions.zotero.ghstar.useAutoCountUpdate', false);
 pref('extensions.zotero.ghstar.useRandomWait', true);
 pref('extensions.zotero.ghstar.randomWaitMinMs', 1000);
 pref('extensions.zotero.ghstar.randomWaitMaxMs', 5000);
-pref('extensions.zotero.ghstar.useSearchTitleFuzzyMatch', false);
-pref('extensions.zotero.ghstar.useSearchAuthorsMatch', true);
-pref('extensions.zotero.ghstar.useDateRangeMatch', false);
 pref(
   'extensions.zotero.ghstar.defaultGithubApiEndpoint',
   'https://api.github.com',

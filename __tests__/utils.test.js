59d1509332f983d3f6ecd936f80c600c265fad06
vitaminac <dalao1002@gmail.com>
2025-10-23 00:27:22 +0200

    fix test error

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -3,8 +3,8 @@ const base = require('../src/ghstar.js');
 describe('Verify $__ghstar.util', () => {
   it('check randomInteger() for proper output ', async () => {
     const test = base.$__ghstar.util.randomInteger(1000, 2000);
-    expect(test).toBeGreaterThan(1000);
-    expect(test).toBeLessThan(2000);
+    expect(test).toBeGreaterThanOrEqual(1000);
+    expect(test).toBeLessThanOrEqual(2000);
   });
   it('check padCountWithZeros() length ', async () => {
     const test = base.$__ghstar.util.padCountWithZeros('1234', 7);
